d9b5c7430bec9d226001585a09a340fdee6c956d
jeff4700 <jeffrey.workspace@gmail.com>
2026-03-23 02:03:35 +0000

    Doc: update requirement

--- merge_test.sh
+++ merge_test.sh
@@ -1,10 +1,8 @@
+export RAY_record_task_actor_creation_sites=true
 mergekit-evolve \
   --storage-path ./evolve_storage \
   examples/evolve_config.yml \
-  --no-vllm \
   --batch-size 4 \
   --task-search-path /root/workspace/mergekit/tasks \
   --num-gpus 4 \
   --i-understand-the-depths-of-the-evils-i-am-unleashing
-
-
